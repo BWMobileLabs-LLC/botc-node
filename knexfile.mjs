7b5d048415ce2898f8a999a8b2adb453d58190e3
Brandon Wallace <brandonmwallace97@gmail.com>
2026-05-05 13:03:10 -0700

    dockerize

--- knexfile.mjs
+++ knexfile.mjs
@@ -4,26 +4,27 @@ import 'dotenv/config';
  * @type { import('knex').Knex.Config }
  */
 const config = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.PGHOST,
-      port: Number(process.env.PGPORT) || 5432,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD || undefined,
-      database: process.env.PGDATABASE,
-    },
-    migrations: {
-      directory: './migrations',
-      extension: 'mjs',
-      loadExtensions: ['.mjs'],
-    },
-    seeds: {
-      directory: './seeds',
-      extension: 'mjs',
-      loadExtensions: ['.mjs'],
-    },
+  client: 'pg',
+  connection: {
+    host: process.env.PGHOST,
+    port: Number(process.env.PGPORT) || 5432,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD || undefined,
+    database: process.env.PGDATABASE,
+  },
+  migrations: {
+    directory: './migrations',
+    extension: 'mjs',
+    loadExtensions: ['.mjs'],
+  },
+  seeds: {
+    directory: './seeds',
+    extension: 'mjs',
+    loadExtensions: ['.mjs'],
   },
 };
 
-export default config;
+export default {
+  development: config,
+  production: config
+};
